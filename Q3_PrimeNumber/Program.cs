8a1c72416dceb0c8241c2534ce91698aba652f98
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-11 22:48:35 +0530

    [prince] Prime Number

---
+++ Q3_PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q3_PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int count = 0; ;
+
+            for(int i=1; i<number; i++)
+            {
+                if(number%i==0)
+                {
+                    count++;    
+                }
+                if(count==2)
+                {
+                    Console.WriteLine("it is prime number");
+                }
+                else
+                {
+                    Console.WriteLine("it is not a prime number");
+                }
+            }
+        }
+    }
+}
