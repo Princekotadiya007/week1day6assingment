94f1ac36d08f0b41f3c08a703b271554ca7cd09e
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-11 22:47:24 +0530

    [prince] Perfect Number

---
+++ Q2_PerfectNumber/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2_PerfectNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("enter the number");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for(int i = 1; i <= number/2; i++)
+            {
+                if(number % i == 0)
+                {
+                    sum += i;
+                }
+                if(sum == number)
+                {
+                    Console.WriteLine("ture");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+        }
+    }
+}
