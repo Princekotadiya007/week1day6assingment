41b19e515e58385a885aaf1211e38730652b241f
Princekotadiya007 <121780771+Princekotadiya007@users.noreply.github.com>
2023-01-12 12:50:28 +0530

    [prince] Reverse Number

---
+++ Q4_ReverseNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4_ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reverse = 0;
+
+            while (number != 0)
+            {
+                int remainder = number % 10;
+                reverse = (reverse * 10) + remainder;
+                number = number / 10;
+            }
+
+            Console.WriteLine("Reverse of the number is: " + reverse);
+        }
+    }
+}
